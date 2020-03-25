f7ad22ad628ac4d8bdd57db7d2fa51c630da6474
Hur <sjin.75@gmail.com>
2020-03-25 15:55:31 +0900

    p12 keyfile -> json keyfile

--- reportingAPI.py
+++ reportingAPI.py
@@ -1,106 +1,104 @@
 """Analytics Reporting API V4."""
 
-#import argparse
-
 from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
-
-import httplib2
-from oauth2client import client
-from oauth2client import file
-from oauth2client import tools
 
 import datetime
 
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
-DISCOVERY_URI = ('https://analyticsreporting.googleapis.com/$discovery/rest')
-KEY_FILE_LOCATION = './client_secrets.p12'
-SERVICE_ACCOUNT_EMAIL = '699053570093-271jrj42cl3tmdmml308ipglmhoq6bih@developer.gserviceaccount.com'
+KEY_FILE_LOCATION = './client_secrets.json'
+
 
 def initialize_analyticsreporting():
-  """Initializes an analyticsreporting service object.
+    """Initializes an Analytics Reporting API V4 service object.
 
-  Returns:
-    analytics an authorized analyticsreporting service object.
-  """
+    Returns:
+      An authorized Analytics Reporting API V4 service object.
+    """
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        KEY_FILE_LOCATION, SCOPES)
 
-  credentials = ServiceAccountCredentials.from_p12_keyfile(
-    SERVICE_ACCOUNT_EMAIL, KEY_FILE_LOCATION, scopes=SCOPES)
+    # Build the service object.
+    analytics = build('analyticsreporting', 'v4', credentials=credentials)
 
-  http = credentials.authorize(httplib2.Http())
-
-  # Build the service object.
-  analytics = build('analytics', 'v4', http=http, discoveryServiceUrl=DISCOVERY_URI)
-
-  return analytics
+    return analytics
 
 
-def get_report(analytics, view_id , date):
-  # Use the Analytics Service Object to query the Analytics Reporting API V4.
-  return analytics.reports().batchGet(
-      body={
-        'reportRequests': [
-        {
-          'viewId': view_id,
-          'dateRanges': [{'startDate': date, 'endDate': date}],
-          'dimensions': [{'name': 'ga:date'}],
-          'metrics': [{'expression': 'ga:sessions'}, 
-                      {'expression': 'ga:users'}, 
-                      {'expression': 'ga:newUsers'}, 
-                      {'expression': 'ga:avgSessionDuration'}, 
-                      {'expression': 'ga:transactions'},
-                      {'expression': 'ga:transactionRevenue', 'formattingType': 'INTEGER'}
-                     ],
-          'samplingLevel':  'LARGE'
-        }]
-      }
-  ).execute()
+def get_report(analytics, view_id, date):
+    """Queries the Analytics Reporting API V4.
+
+    Args:
+      analytics: An authorized Analytics Reporting API V4 service object.
+    Returns:
+      The Analytics Reporting API V4 response.
+    """
+    return analytics.reports().batchGet(
+        body={
+            'reportRequests': [
+            {
+              'viewId': view_id,
+              'dateRanges': [{'startDate': date, 'endDate': date}],
+              'metrics': [{'expression': 'ga:sessions'},
+                          {'expression': 'ga:users'},
+                          {'expression': 'ga:newUsers'},
+                          {'expression': 'ga:avgSessionDuration'},
+                          {'expression': 'ga:transactions'},
+                          {'expression': 'ga:transactionRevenue', 'formattingType': 'INTEGER'}
+                         ],
+              'dimensions': [{'name': 'ga:date'}],
+              'samplingLevel':  'LARGE'
+            }]
+        }
+    ).execute()
 
 
 def process_response(response):
-  """stores the Analytics Reporting API V4 response"""
+    """Parses and prints the Analytics Reporting API V4 response.
 
-  for report in response.get('reports', []):
-    columnHeader = report.get('columnHeader', {})
-    dimensionHeaders = columnHeader.get('dimensions', [])
-    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
-    rows = report.get('data', {}).get('rows', [])
-    samplesReadCounts = report.get('data', {}).get('samplesReadCounts', [])
-    metrics = {}
+    Args:
+      response: An Analytics Reporting API V4 response.
+    """
+    for report in response.get('reports', []):
+        columnHeader = report.get('columnHeader', {})
+        dimensionHeaders = columnHeader.get('dimensions', [])
+        metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+        samplesReadCounts = report.get('data', {}).get('samplesReadCounts', [])
+        result = {}
 
-    for row in rows:
-      dateRangeValues = row.get('metrics', [])
+        for row in report.get('data', {}).get('rows', []):
+            dimensions = row.get('dimensions', [])
+            dateRangeValues = row.get('metrics', [])
 
-      for i, values in enumerate(dateRangeValues):
-        for metricHeader, value in zip(metricHeaders, values.get('values')):
-          metrics[metricHeader.get('name')] = value
+            for header, dimension in zip(dimensionHeaders, dimensions):
+                result[header] = dimension
 
-      for date in row.get('dimensions', []):
-        metrics['date'] = date
+            for i, values in enumerate(dateRangeValues):
+                for metricHeader, value in zip(metricHeaders, values.get('values')):
+                    result[metricHeader.get('name')] = value
 
-    if len(samplesReadCounts) == 0:
-      metrics['sampled'] = 'no'
-    else: 
-      metrics['sampled'] = 'yes'
+        if len(samplesReadCounts) == 0:
+            result['sampled'] = 'no'
+        else:
+            result['sampled'] = 'yes'
 
-    return metrics
+    return result
+
 
 def main():
+    analytics = initialize_analyticsreporting()
 
-  analytics = initialize_analyticsreporting()
+    date_start = datetime.date(2020, 3, 1)
+    date_range = 2
 
-  date_start = datetime.date(2017, 1, 5)
-  date_range = 2
+    view_ids = ['89357101', '102782120', '88823264', '84826225']
 
-  view_ids = ['89357101', '102782120', '88823264', '84826225']
+    for view_id in view_ids:
+        print(view_id)
+        for date in [date_start + datetime.timedelta(days=n) for n in range(0, date_range, 1)]:
+            response = get_report(analytics, view_id, date.isoformat())
+            result = process_response(response)
+            print(result)
 
-  for view_id in view_ids:
-    print(view_id)
-    for date in [date_start + datetime.timedelta(days=n) for n in range(0, date_range, 1)]:
-      response = get_report(analytics, view_id, date.isoformat())
-      result = process_response(response)
-      print(result)
 
 if __name__ == '__main__':
-  main()
-
+    main()
